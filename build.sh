06a4611f977320408bc87f17f77e2ae9395e7422
Rohit Kumar Singh <77824864+notrohit1702@users.noreply.github.com>
2025-06-22 08:48:20 +0530

    Update build.sh
    Revert all changes

--- build.sh
+++ build.sh
@@ -165,7 +165,9 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 "${BUILD_DIR}")" ]; then abort "All builds failed."; fi
 
-log "\nInstall [MicroG-RE](https://github.com/WSTxda/MicroG-RE/releases) for YouTube and YouTube Music"
+log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music APKs"
+log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root ReVanced YouTube and YT Music from Play Store"
+log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)\n"
 log "$(cat "$TEMP_DIR"/*-rv/changelog.md)"
 
 SKIPPED=$(cat "$TEMP_DIR"/skipped 2>/dev/null || :)
