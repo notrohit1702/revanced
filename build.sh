6e75630b42c6310454ee6dbd03503e95c8f2b9df
Rohit Kumar Singh <77824864+notrohit1702@users.noreply.github.com>
2025-10-09 22:29:34 +0530

    Update build.sh
    Sync commit 6a4f880 from upstream

--- build.sh
+++ build.sh
@@ -130,7 +130,7 @@ for table_name in $(toml_get_table_names); do
 		abort "wrong arch '${app_args[arch]}' for '$table_name'"
 	fi
 
-	app_args[include_stock]=$(toml_get "$t" include-stock) || app_args[include_stock]=true && vtf "${app_args[include_stock]}" "include-stock"
+	app_args[dpi]=$(toml_get "$t" dpi) || app_args[dpi]="nodpi"
 	app_args[dpi]=$(toml_get "$t" apkmirror-dpi) || app_args[dpi]="nodpi"
 	table_name_f=${table_name,,}
 	table_name_f=${table_name_f// /-}
