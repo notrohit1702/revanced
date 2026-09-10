b81079cdcf342db0ba2baf5178365d77047258f9
Rohit Kumar Singh <77824864+notrohit1702@users.noreply.github.com>
2026-09-10 21:16:46 +0530

    Change release footer

--- build.sh
+++ build.sh
@@ -160,9 +160,7 @@ wait
 _clean_tmp
 if [ -z "$(ls -A1 "${BUILD_DIR}")" ]; then abort "All builds failed."; fi
 
-log "\nInstall [Microg](https://github.com/MorpheApp/MicroG-RE/) for non-root YouTube and YT Music APKs"
-log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach YouTube and YT Music modules from Play Store"
-log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)\n"
+log "\nInstall [MicroG RE](https://github.com/MorpheApp/MicroG-RE/releases) for YouTube and YouTube Music.\n"
 log "$(cat "$TEMP_DIR"/*/changelog.md)"
 
 SKIPPED=$(cat "$TEMP_DIR"/skipped 2>/dev/null || :)
